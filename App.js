5a11e804b3bc5c1ccfeb87a69452d819b95f907e
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-05-09 17:09:10 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import "react-native-gesture-handler";
+import { Provider } from "react-redux";
 
 import { UserProvider } from "./userContext";
+import { store } from "./redux/store";
 
 import MainComponent from "./src/components/MainComponent";
 
 const App = () => {
   return (
-    <UserProvider>
-      <MainComponent />
-    </UserProvider>
+    <Provider store={store}>
+      <UserProvider>
+        <MainComponent />
+      </UserProvider>
+    </Provider>
   );
 };
 
